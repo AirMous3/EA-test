2cb01354d51570810a9320949a616088cf97ef5e
AirMouse <airmouselol@gmail.com>
2023-05-22 16:31:51 +0300

    chore: small changes

--- src/entities/eventlist/Eventlist.jsx
+++ src/entities/eventlist/Eventlist.jsx
@@ -14,35 +14,33 @@ export const EventList = () => {
           {eventsConfig.map(({ img, title, date, id }) => {
             const active = isActive === id;
             return (
-              <S.AccordionItem  $isActive={active} img={img} key={id}>
+              <S.AccordionItem $isActive={active} img={img} key={id}>
                 <S.AccordionTitleWrapper
-                    $isActive={active}
+                  $isActive={active}
                   onClick={() => setActive(id)}
                 >
                   <S.AccordionTitle>{title}</S.AccordionTitle>
                   <S.AccordionNumber>0{id}</S.AccordionNumber>
                 </S.AccordionTitleWrapper>
 
-                {active && (
-                  <S.AccordionInfoWrapper>
-                    <S.AccordionMInfoWrapper>
-                      <S.AccordionInfoNumber>0{id}</S.AccordionInfoNumber>
-                      <S.AccordionInfoTitle>{title}</S.AccordionInfoTitle>
-                      <S.AccordionInfoDate>{date}</S.AccordionInfoDate>
+                <S.AccordionInfoWrapper>
+                  <S.AccordionMInfoWrapper>
+                    <S.AccordionInfoNumber>0{id}</S.AccordionInfoNumber>
+                    <S.AccordionInfoTitle>{title}</S.AccordionInfoTitle>
+                    <S.AccordionInfoDate>{date}</S.AccordionInfoDate>
 
-                      <S.AccordionButton>
-                        {/*  Не знаю куда должна перенаправлять кнопка, по тз не сказано, поэтому пусть будет так)*/}
-                        <a
-                          href="https://egorovagency.by/about#about-team"
-                          rel="noopener noreferrer"
-                          target="_blank"
-                        >
-                          More information
-                        </a>
-                      </S.AccordionButton>
-                    </S.AccordionMInfoWrapper>
-                  </S.AccordionInfoWrapper>
-                )}
+                    <S.AccordionButton>
+                      {/*  Не знаю куда должна перенаправлять кнопка, по тз не сказано, поэтому пусть будет так)*/}
+                      <a
+                        href="https://egorovagency.by/about#about-team"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        More information
+                      </a>
+                    </S.AccordionButton>
+                  </S.AccordionMInfoWrapper>
+                </S.AccordionInfoWrapper>
               </S.AccordionItem>
             );
           })}
